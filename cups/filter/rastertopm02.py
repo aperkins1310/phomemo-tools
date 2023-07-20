df988369978e7df0e66164af9fb2c6d782c86fe0
Adam Perkins <aperkins1310@gmail.com>
2023-07-20 17:21:40 +0100

    Use different bytes-per-line

--- cups/filter/rastertopm02.py
+++ cups/filter/rastertopm02.py
@@ -79,12 +79,13 @@ def print_raster(file, image, line, lines = 0xff, mode = 0):
     file.write(GS + b'v0')   # GS v 0 : print raster bit image
     # 0: normal, 1 double width, 2 double heigh, 3 quadruple
     file.write(mode.to_bytes(1, 'little'))
-    # number of bytes / line
-    file.write(int((image.width + 7) / 8).to_bytes(2, 'little'))
+
+    printable_width = 576
+    bytes_per_line = (printable_width + 7) // 8
+    file.write((bytes_per_line).to_bytes(2, 'little'))
      # Number of lines in the image
     file.write(lines.to_bytes(2, 'little'))
 
-    printable_width = 576
     right_padding = max(0, printable_width - image.width)
     # Create a padded image with right alignment
     block = image.crop((0, line, image.width, line + lines))
